a9ab760ecf51f012649ffd5f812e9a072aba788b
weld <weld.lab@outlook.com>
2026-03-12 10:17:42 +0100

    parsing het record

--- src/reader.util.lisp
+++ src/reader.util.lisp
@@ -4,25 +4,48 @@
 (defparameter *test-file*
   (merge-pathnames (pathname "git/cl-pdb/src/8ef5.pdb")))
 
+(defparameter *test-het*
+  "HET    7V7  R 501      25                                                       ")
+
 
 (defmacro is-record-type (str)
   `(equalp record-type ,str))
 
 
-(defun parse-remark (remark)
-  (print remark))
+(defun parse-het (het)
+  ;;; HET record
+  ;;; 1-6    : record name ("HET")
+  ;;; 8-10   : hetID       -- het identifier, right-justified
+  ;;; 13     : chainID     -- chain identifier
+  ;;; 14-17  : seqNum      -- sequence number
+  ;;; 18     : iCode       -- insertion code
+  ;;; 21-25  : numHetAtoms -- number of HETATM records for this group
+  ;;; 31-70  : text        -- text describing the het group
+  (let ((hetID (subseq het 7 10))
+	(chainID (subseq het 12 13))
+	(seqNum (parse-integer (subseq het 13 17))))
+    (make-instance 'residue
+		   :residue-name hetID
+		   :residue-id seqNum
+		   :residue-chain chainID
+		   :residue-kind :unknown)))
 
+(defun parse-remark (remark)
+  nil)
 
 (defun parse-hetatm (hetatm)
-  (print hetatm))
+  nil)
 
 
 (defun parse-record (record)
   (let ((record-type (subseq record 0 6)))
-    (cond
-      ((is-record-type "REMARK") (parse-remark record))
-      ((is-record-type "HETATM") (parse-hetatm record))
-      (t nil))))
+    (let  ((things '()))
+      (cond
+	((is-record-type "HET   ") (push (parse-het record) things))
+	((is-record-type "REMARK") (parse-remark record))
+	((is-record-type "HETATM") (parse-hetatm record))
+	(t nil))
+      things)))
 
 
 (defun reader (pathname)
--- src/residue.class.lisp
+++ src/residue.class.lisp
@@ -1,11 +1,29 @@
 (in-package #:cl-pdb)
 
 
+; quand il faudra normaliser, ça va être important de prendre
+; des pdb et simplement de voir tous ceux qui sont marqués ":unknown"
+; et de remplir à la main gentiment
+
+
+
 (defclass residue ()
-  ;; kind c'est le type "métier"
-  ;; "structural" ; par exemple, :water, :ligand, :amino-acid, :other
-  ;; mais pas un kind chimique :CLR
-  ;; faudra faire une normalisation pour attribuer les kind
-  ((kind :initarg :kind
-	 :initform :unknown
-	 :accessor residue-kind)))
+  ((residue-name :initarg :residue-name
+		 :initform nil
+		 :accessor residue-name
+		 :documentation "Contient le nom du résidu. Typiquement, le code à trois caractères 7V7 qui identifie le fentanyl.")
+   (residue-id :initarg :residue-id ; c'est également un seqNum pour les het
+	       :initform nil
+	       :accessor residue-id
+	       :documentation "Pas sûr pour le moment. Je mettrais bien le seqNum ici pour les hetero atoms.")
+   (residue-chain :initarg :residue-chain
+		  :initform nil
+		  :accessor residue-chain
+		  :documentation "Nom de la chaîne.")
+   (residue-kind :initarg :residue-kind
+		 :initform :unknown
+		 :accessor residue-kind
+		 :documentation "Contient un keyword 'métier', par exemple :water, :ligand ou :amino-acids. On a :other pour ceux qui n'entrent pas dans les catégories à définir, et :unknown quand pas encore défini. Faudra bien sûr passer par une étape de normalisation ; ex H2O et HOH entrent dans le residue-kind :water.")
+   (residue-atoms :initform '()
+		  :accessor residue-atoms))
+  (:documentation "Classe qui représente un résidu, au sens structural."))
