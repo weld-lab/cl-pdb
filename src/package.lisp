7f391199f8cad859589643c80c9ea2992c5cc92d
weld <weld.lab@outlook.com>
2026-03-13 08:39:10 +0100

    export symbols

--- src/package.lisp
+++ src/package.lisp
@@ -1,6 +1,21 @@
 (defpackage #:cl-pdb
-  (:use :cl)
-  (:shadow :atom))
+  (:nicknames #:pdb)
+  (:use #:cl)
+  (:shadow #:atom)
+  (:export #:pdb
+	   #:pdb-tile
+	   #:pdb-sequence
+	   #:pdb-residues
+	   #:residue
+	   #:residue-name
+	   #:residue-id
+	   #:residue-sequence-number
+	   #:residue-insertion-code
+	   #:residue-chain
+	   #:residue-kind
+	   #:residue-atoms
+	   #:residue-additional-informations
+	   #:atom))
 
 
 
