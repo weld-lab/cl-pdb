1c5b61c93d15a9d905a89f3e6aa1a9c35936db13
weld <weld.lab@outlook.com>
2026-03-13 09:47:27 +0100

    update reader

--- src/reader.util.lisp
+++ src/reader.util.lisp
@@ -72,7 +72,7 @@
 	(x (parse-float (subseq atom 30 38)))
 	(y (parse-float (subseq atom 38 46)))
 	(z (parse-float (subseq atom 46 54)))
-	(charge (subseq atom 78 80)))
+	(charge (parse-float (subseq atom 78 80))))
     (make-instance 'atom
 		   :atom-name name
 		   :atom-serial serial
