d39f75b78279d7f64ed41e9b5229bd59dbd4027b
weld <weld.lab@outlook.com>
2026-03-13 09:50:41 +0100

    parse-float handles nil

--- src/reader.util.lisp
+++ src/reader.util.lisp
@@ -14,13 +14,18 @@
 
 
 ; utils
-(defun parse-float (str)
-  (car (multiple-value-list (read-from-string str))))
 
 (defun trim-or-nil (str)
   (let ((s (string-trim '(#\Space #\Tab #\Newline) str)))
     (unless (string= s "")
       s)))
+
+
+(defun parse-float (str)
+  (let ((str (trim-or-nil str)))
+    (if (null str)
+	nil
+	(car (multiple-value-list (read-from-string str))))))
 
 
 (defun parse-het (het)
