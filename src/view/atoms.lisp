e0641f2806054494281ad9585dcc31c06b976149
weld <weld.lab@outlook.com>
2026-03-19 12:34:32 +0100

    some views

--- src/view/atoms.lisp
+++ src/view/atoms.lisp
@@ -1,1 +1,17 @@
 (in-package #:cl-pdb)
+
+
+(defun atoms-of-residue (residue)
+  (residue-atoms residue))
+
+
+(defun atoms-in-chain (residues chainID)
+  (loop for residue in residues
+        when (string= chainID (residue-chain residue))
+          append (residue-atoms residue)))
+
+
+(defun atoms-of-kind (residues kind)
+  (loop for residue in residues
+        when (eql kind (residue-kind residue))
+          append (residue-atoms residue)))
--- src/view/residues.lisp
+++ src/view/residues.lisp
@@ -12,3 +12,19 @@
   (loop for residue in residues
 	when (eql kind (residue-kind residue))
 	collect residue))
+
+
+(defun residues-named (residues name)
+  (loop for residue in residues
+        when (string= name (residue-name residue))
+          collect residue))
+
+(defun hetero-residues (residues)
+  (loop for residue in residues
+        when (eql :hetero (residue-type residue))
+        collect residue))
+
+(defun unknown-residues (residues)
+  (loop for residue in residues
+        when (eql :unknown (residue-kind residue))
+        collect residue))
