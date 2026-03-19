233892e4f089ba23d35ae45d376a323e302a99b2
weld <weld.lab@outlook.com>
2026-03-19 11:58:15 +0100

    moved old files

--- src/view/residues.lisp
+++ src/view/residues.lisp
@@ -1,1 +1,14 @@
 (in-package #:cl-pdb)
+
+
+
+(defun residues-in-chain (residues chainID)
+  (loop for residue in residues
+	when (string= chainID (residue-chain residue))
+	collect residue))
+
+
+(defun residues-of-kind (residues kind)
+  (loop for residue in residues
+	when (eql kind (residue-kind residue))
+	collect residue))
