00fed0679f764f83bd3f1b186e1518eefb61436d
weld <weld.lab@outlook.com>
2026-03-19 12:26:13 +0100

    plural pipelines

--- src/view/pipe.lisp
+++ src/view/pipe.lisp
@@ -1,9 +1,20 @@
 (in-package #:cl-pdb)
 
 
-(defmacro pipe (pdb &body forms)
-  `(let ((it (pdb-residues ,pdb)))
+(defmacro pipe (initial-value &body forms)
+  `(let ((it ,initial-value))
      ,@(mapcar (lambda (form)
                  `(setf it ,form))
                forms)
      it))
+
+
+(defmacro pipe-residues (pdb &body forms)
+  `(pipe (pdb-residues ,pdb)
+     ,@forms))
+
+
+(defmacro pipe-atoms (pdb &body forms)
+  `(pipe (loop for residue in (pdb-residues ,pdb)
+               append (residue-atoms residue))
+     ,@forms))
