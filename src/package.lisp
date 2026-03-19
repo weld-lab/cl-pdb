40bfd74a905318a7aaab631cbf9d516c09fea9c2
weld <weld.lab@outlook.com>
2026-03-19 12:42:26 +0100

    sanitizing the exports

--- src/package.lisp
+++ src/package.lisp
@@ -2,33 +2,63 @@
   (:nicknames #:pdb)
   (:use #:cl)
   (:shadow #:atom)
-  (:export #:pdb
-	   #:pdb-title
-	   #:pdb-sequence
-	   #:pdb-residues
+  (:export
+   ;; model
+   #:pdb
+   #:pdb-title
+   #:pdb-sequence
+   #:pdb-residues
 
-	   #:residue
-	   #:residue-name
-	   #:residue-id
-	   #:residue-sequence-number
-	   #:residue-insertion-code
-	   #:residue-chain
-	   #:residue-kind
-	   #:residue-atoms
-	   #:residue-additional-informations
+   #:residue
+   #:residue-name
+   #:residue-id
+   #:residue-sequence-number
+   #:residue-insertion-code
+   #:residue-chain
+   #:residue-kind
+   #:residue-atoms
+   #:residue-additional-informations
 
-	   #:atom
-	   #:atom-name
-	   #:atom-serial
-	   #:atom-residue-name
-	   #:atom-residue-chain
-	   #:atom-residue-sequence-number
-	   #:atom-residue-insertion-code
-	   #:atom-x
-	   #:atom-y
-	   #:atom-z
-	   #:atom-element
-	   #:atom-charge))
+   #:atom
+   #:atom-name
+   #:atom-alternative-location
+   #:atom-serial
+   #:atom-residue-name
+   #:atom-residue-chain
+   #:atom-residue-sequence-number
+   #:atom-residue-insertion-code
+   #:atom-x
+   #:atom-y
+   #:atom-z
+   #:atom-occupancy
+   #:atom-temperature-factor
+   #:atom-element
+   #:atom-charge
+
+   ;; read / write
+   #:read-pdb
+   #:write-pdb
+   #:write-residues
+
+   ;; normalization
+   #:infer-residue-kind
+
+   ;; residue views
+   #:residues-in-chain
+   #:residues-of-kind
+   #:residues-named
+   #:hetero-residues
+   #:unknown-residues
+
+   ;; atom views
+   #:atoms-of-residue
+   #:atoms-in-chain
+   #:atoms-of-kind
+
+   ;; pipes
+   #:pipe
+   #:pipe-residues
+   #:pipe-atoms))
 
 
 
